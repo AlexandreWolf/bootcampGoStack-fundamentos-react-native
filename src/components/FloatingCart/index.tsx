6f82092123bfc87ea639649e3a69e9cc434c914b
Alexandre Guidio Wolf <alexandregw1@gmail.com>
2020-10-03 17:46:59 -0300

    Add challenge codes

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { useNavigation } from '@react-navigation/native';
 
@@ -15,24 +15,27 @@ import formatValue from '../../utils/formatValue';
 
 import { useCart } from '../../hooks/cart';
 
-// Calculo do total
-// Navegação no clique do TouchableHighlight
-
 const FloatingCart: React.FC = () => {
   const { products } = useCart();
 
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const productsSubtotal = product.price * product.quantity;
+      return acc + productsSubtotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, product) => {
+      const productsQuantity = product.quantity;
+      return acc + productsQuantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
